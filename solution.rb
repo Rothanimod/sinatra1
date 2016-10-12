73a6034f3dd960ea1eb41fc0a5af84eb608b26fd
Mauricio Noreña <baphometh@gmail.com>
2016-10-11 20:25:13 -0500

    Fixed nil response for name

--- solution.rb
+++ solution.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  "<h1>Hola #{params[:name]}</h1>"
+  unless params[:name]
+    "<h1>Hola desconocido!</h1>"
+  else
+    "<h1>Hola #{params[:name]}</h1>"
+  end
 end
